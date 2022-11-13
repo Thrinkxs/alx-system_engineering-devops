d629c4c13252ec6193a4f8a7644c71b9e45a3a5a
Emmanuel <emmanuelozigue@gmail.com>
2022-11-13 10:11:57 -0800

    attempt

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -8,12 +8,12 @@
  *
  * Return: Always 0.
  */
-int infinite_while(void) 
-{  
+int infinite_while(void)
+{
 while (1)
-{      
+{
 sleep(1);
-}  
+}
 return (0);
 }
 
@@ -22,9 +22,9 @@ return (0);
  *
  * Return: Always 0.
  */
-int main(void)  
+int main(void)
 {
-pid_t pid;  
+pid_t pid;
 char count = 0;
 while (count < 5)
 {
@@ -32,12 +32,12 @@ pid = fork();
 if (pid > 0)
 {
 printf("Zombie process created, PID: %d\n", pid);
-sleep(1);	  
-count++;	  
-}      
-else	
+sleep(1);
+count++;
+}
+else
 exit(0);
 }
 infinite_while();
-return (EXIT_SUCCESS); 
+return (EXIT_SUCCESS);
 }
